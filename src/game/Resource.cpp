0bc72d954cff34a290ff00edd67f1f1929450887
ArthurSonzogni <sonzogniarthur@gmail.com>
2020-03-09 21:50:14 +0100

    Fix ResourcePath for snap.

--- src/game/Resource.cpp
+++ src/game/Resource.cpp
@@ -25,7 +25,7 @@ std::string ResourcePath() {
 
   std::vector<std::string> path_list = {
       // Application installed using snapcraft.
-      SNAP + "/usr/share/inthecube/resources",
+      SNAP + "/share/icemaze/resources",
 
       // Application installed using "sudo make install"
       "/usr/share/inthecube/resources",
